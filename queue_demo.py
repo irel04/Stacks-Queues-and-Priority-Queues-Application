eedc8be2c63b58df521bd41b5fabcfa123962b76
Mark Lhester Agellon <kianirel56@gmail.com>
2022-12-13 11:49:21 +0800

    Running Queue DONE

--- queue_demo.py
+++ queue_demo.py
@@ -1,8 +1,9 @@
 from queues import Queue
 
 fifo = Queue("1st", "2nd", "3rd", "4th", "5th")
-print(len(fifo))
+print("\n", len(fifo), "\n")
 
 for data in fifo:
     print(data)
 
+print("\n", len(fifo))
