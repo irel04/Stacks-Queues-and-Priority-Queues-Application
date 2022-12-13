95d421c1597985989e7cbf07214eff5cea5f8f04
Mark Lhester Agellon <kianirel56@gmail.com>
2022-12-13 15:17:12 +0800

    Variables added and methods are being used

--- Priority_Queue_Data_Type/queues.py
+++ Priority_Queue_Data_Type/queues.py
@@ -1,8 +1,12 @@
 from collections import deque
 from heapq import heappush, heappop
+
 class PriorityQueue:
     def __init__(self):
         self._elements =[]
     
     def enqueue_with_priority(self, priority, value):
         heappush(self._elements, (priority, value))
+
+    def deqeue(self):
+        return heappop(self._elements)[1]
