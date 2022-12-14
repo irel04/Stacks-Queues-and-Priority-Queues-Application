f9a9f007b8a028d5b303741584f2fb83f90ffa2c
Mark Lhester Agellon <kianirel56@gmail.com>
2022-12-14 16:05:16 +0800

    Call Data Class

--- Handling_Corner_Cases/Handling_Corner_Cases_Demo.py
+++ Handling_Corner_Cases/Handling_Corner_Cases_Demo.py
@@ -6,8 +6,6 @@ IMPORTANT = 2
 NEUTRAL = 1
 
 messages = PriorityQueue()
-messages.enqueue_with_priority(IMPORTANT, "Windshield wipers turned on")
-messages.enqueue_with_priority(NEUTRAL, "Radio station tuned in")
 messages.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
 messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")
 
