3f685ef34e161c3619a03aee478db8e5597f33a1
Mark Lhester Agellon <kianirel56@gmail.com>
2022-12-14 16:08:04 +0800

    compare two classes

--- Handling_Corner_Cases/Handling_Corner_Cases_Demo.py
+++ Handling_Corner_Cases/Handling_Corner_Cases_Demo.py
@@ -16,6 +16,9 @@ class Message:
 wipers = Message("Windshield wipers turned on")
 hazard_lights = Message("Hazard lights turned on")
 
+wipers < hazard_lights
+
+
 
 print("\n")
 print(messages.dequeue())
