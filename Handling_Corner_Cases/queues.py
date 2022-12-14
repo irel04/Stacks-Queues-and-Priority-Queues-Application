05c60949cb42ead34f7cd1ece2cd429b87439168
Mark Lhester Agellon <kianirel56@gmail.com>
2022-12-14 15:43:06 +0800

    Added class Priority queue and declare new method

--- Handling_Corner_Cases/queues.py
+++ Handling_Corner_Cases/queues.py
@@ -1,3 +1,8 @@
 from collections import deque
 from heapq import heappop, heappush
 from itertools import count
+
+class PriorityQueue:
+    def __init__(self):
+        self._elements = []
+        self._counter = count()
