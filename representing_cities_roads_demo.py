6889ea3188d240f94b95ff0e25046421cd131132
Mark Lhester Agellon <kianirel56@gmail.com>
2022-12-16 06:23:13 +0800

    Demo -> graph.nodes

--- representing_cities_roads_demo.py
+++ representing_cities_roads_demo.py
@@ -3,6 +3,9 @@ import networkx as nx
 from graph import City, load_graph
 
 # Roadmap demo
-print(nx.nx_agraph.read_dot("roadmap.dot"))
+print("\n", nx.nx_agraph.read_dot("roadmap.dot"))
 
+# Viewing imported CLASS
+graph = nx.nx_agraph.read_dot("roadmap.dot")
+print("\n\n", graph.nodes["london"])
 
