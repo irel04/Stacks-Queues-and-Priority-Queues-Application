050961f3284c0fc332609a30529a96ff17da2864
Mark Lhester Agellon <kianirel56@gmail.com>
2022-12-15 22:18:13 +0800

    Import-Class: City and Method: load_graph

--- representing_cities_roads_demo.py
+++ representing_cities_roads_demo.py
@@ -1,4 +1,7 @@
 import networkz as nx
+from graph import City, load_graph
+
+# Roadmap demo
 print(nx.nx_agraph.read_dot("roadmap.dot"))
 graph = nx.nx_agraph.read_dot("roadmap.dot")
 graph.nodes["london"]
