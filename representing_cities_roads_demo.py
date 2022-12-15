d3b096879d7fe361306c67c174d12a171127b632
Mark Lhester Agellon <kianirel56@gmail.com>
2022-12-15 22:21:07 +0800

    Demo script: Representing Graph of London

--- representing_cities_roads_demo.py
+++ representing_cities_roads_demo.py
@@ -5,3 +5,9 @@ from graph import City, load_graph
 print(nx.nx_agraph.read_dot("roadmap.dot"))
 graph = nx.nx_agraph.read_dot("roadmap.dot")
 graph.nodes["london"]
+
+# Using Class -> City() and Method -> load_graph from the graph.py module
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+
+nodes["london"]
+print(graph)
