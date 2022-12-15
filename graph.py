691096f1b153731274ab3749eda9c8e68fc1583e
Mark Lhester Agellon <kianirel56@gmail.com>
2022-12-15 19:10:34 +0800

    +Method -> load_graph(filename, node_factory)

--- graph.py
+++ graph.py
@@ -8,6 +8,7 @@ class City(NamedTuple):
     latitude: float
     longitude: float
 
+    # For storing data in dict
     @classmethod
     def from_dict(cls, attrs):
         return cls(
@@ -17,3 +18,15 @@ class City(NamedTuple):
             latitude=float(attrs["latitude"]),
             longitude=float(attrs["longitude"]),
         )
+    
+    # A graph instance that take note of the mapping of node identifiers to city instances
+    def load_graph(filename, node_factory):
+        graph = nx.nx_agraph.read_dot(filename)
+        nodes = {
+            name: node_factory(attributes)
+            for name, attributes in graph.nodes(data=True)
+        }
+        return nodes, nx.Graph(
+            (nodes[name1], nodes[name2], weights)
+            for name1, name2, weights in graph.edges(data=True)
+    )
