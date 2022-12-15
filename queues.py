ae350d42cbdd7b9c6fd81cd653e31f2f56fdc2a7
Mark Lhester Agellon <kianirel56@gmail.com>
2022-12-15 10:17:21 +0800

    Added __iter__ method

--- queues.py
+++ queues.py
@@ -44,3 +44,7 @@ class IterableMixin:
     def __len__(self):
         return len(self._elements)
     
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
+    
