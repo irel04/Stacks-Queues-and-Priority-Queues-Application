0609eba666d87b79a8eab059d0006c4d73a000ff
Mark Lhester Agellon <kianirel56@gmail.com>
2022-12-16 10:19:57 +0800

    Implementing shortest path using bfs

--- Shortest_Path_BFS.py
+++ Shortest_Path_BFS.py
@@ -3,3 +3,10 @@ from graph import City, load_graph
 
 # instantiating nodes and graphs
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
+
+city1 = nodes["aberdeen"]
+city2 = nodes["perth"]
+
+# Testing shortest path using bfs
+for i, path in enumerate(nx.all_shortest_paths(graph, city1, city2), 1):
+    print(f"{i}.", " → ".join(city.name for city in path))
