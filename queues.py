4a0d28a62b72103c58987ca376c6e7541ee65260
Mark Lhester Agellon <kianirel56@gmail.com>
2022-12-16 13:04:08 +0800

    +method -> __getitem__()

--- queues.py
+++ queues.py
@@ -71,3 +71,6 @@ class MutableMinHeap(IterableMixin):
             element = Element(priority, next(self._counter), unique_value)
             self._elements_by_value[unique_value] = element
             heappush(self._elements, element)
+
+    def __getitem__(self, unique_value):
+        return self._elements_by_value[unique_value].priority
