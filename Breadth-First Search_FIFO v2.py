6e801a855ab7e5bc7d80505db7b50e3f2e87714e
Mark Lhester Agellon <kianirel56@gmail.com>
2022-12-16 10:04:03 +0800

    +def -> is_twentiet_century() and instantiante of the breadth_first_search

--- Breadth-First Search_FIFO v2.py
+++ Breadth-First Search_FIFO v2.py
@@ -4,3 +4,12 @@ from graph import (
     breadth_first_traverse,
     breadth_first_search as bfs,
 )
+
+# Function that return year of a city
+def is_twentieth_century(city):
+    return city.year and 1901 <= city.year <= 2000
+
+# This is where the implementation of bread_first_search 
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+city = bfs(graph, nodes["edinburgh"], is_twentieth_century)
+city.name
