918269a74a209e70b5ded7556b3fca2ad229c48d
Mark Lhester Agellon <kianirel56@gmail.com>
2022-12-16 12:59:59 +0800

    +class -> Element

--- queues.py
+++ queues.py
@@ -50,4 +50,8 @@ class PriorityQueue(IterableMixin):
     def dequeue(self):
         return heappop(self._elements)[-1]
 
-    
+@dataclass(order=True)
+class Element:
+    priority: float
+    count: int
+    value: Any
