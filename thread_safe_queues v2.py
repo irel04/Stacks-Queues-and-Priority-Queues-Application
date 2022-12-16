da64b19182b706e6732846c625b78b47749f742a
Mark Lhester Agellon <kianirel56@gmail.com>
2022-12-16 15:25:04 +0800

    create v2 of thread_safe_queues

--- thread_safe_queues v2.py
+++ thread_safe_queues v2.py
@@ -3,6 +3,9 @@ from queue import LifoQueue, PriorityQueue, Queue
 import threading
 from random import choice, randint
 from time import sleep
+from dataclasses import dataclass, field
+from enum import IntEnum
+
 
 # VIEW CLASS
 from itertools import zip_longest
@@ -181,11 +184,24 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         pass
 
+@dataclass(order=True)
+class Product:
+    priority: int
+    label: str = field(compare=False)
 
+    def __str__(self):
+        return self.label
 
+class Priority(IntEnum):
+    HIGH = 1
+    MEDIUM = 2
+    LOW = 3
 
-
-
+PRIORITIZED_PRODUCTS = (
+    Product(Priority.HIGH, ":1st_place_medal:"),
+    Product(Priority.MEDIUM, ":2nd_place_medal:"),
+    Product(Priority.LOW, ":3rd_place_medal:"),
+)
 
 
 
