54d80b0527c091ec6ba2c8a089102b345535e8b5
Mark Lhester Agellon <kianirel56@gmail.com>
2022-12-16 13:10:19 +0800

    +def -> dijkstra_shortest_path()

--- graph.py
+++ graph.py
@@ -122,3 +122,23 @@ def recursive_depth_first_traverse(graph, source, order_by=None):
 def depth_first_search(graph, source, predicate, order_by=None):
     return search(depth_first_traverse, graph, source, predicate, order_by)
 
+def dijkstra_shortest_path(graph, source, destination, weight_factory):
+    previous = {}
+    visited = set()
+
+    unvisited = MutableMinHeap()
+    for node in graph.nodes:
+        unvisited[node] = infinity
+    unvisited[source] = 0
+
+    while unvisited:
+        visited.add(node := unvisited.dequeue())
+        for neighbor, weights in graph[node].items():
+            if neighbor not in visited:
+                weight = weight_factory(weights)
+                new_distance = unvisited[node] + weight
+                if new_distance < unvisited[neighbor]:
+                    unvisited[neighbor] = new_distance
+                    previous[neighbor] = node
+
+    return retrace(previous, source, destination)
