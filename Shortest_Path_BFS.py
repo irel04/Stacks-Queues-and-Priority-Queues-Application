651c1146528242954c14125f59eb93bd98115c2a
Mark Lhester Agellon <kianirel56@gmail.com>
2022-12-16 10:29:56 +0800

    import: shortest_path

--- Shortest_Path_BFS.py
+++ Shortest_Path_BFS.py
@@ -1,5 +1,5 @@
 import networkx as nx
-from graph import City, load_graph
+from graph import City, load_graph, shortest_path
 
 # instantiating nodes and graphs
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
