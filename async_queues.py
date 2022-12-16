8a8129de14a7844e47bf06c21791582425234959
Mark Lhester Agellon <kianirel56@gmail.com>
2022-12-16 16:40:11 +0800

    +def -> fetch_html()

--- async_queues.py
+++ async_queues.py
@@ -27,3 +27,10 @@ def display(links):
 
 if __name__ == "__main__":
     asyncio.run(main(parse_args()))
+
+
+async def fetch_html(session, url):
+    async with session.get(url) as response:
+        if response.ok and response.content_type == "text/html":
+            return await response.text()
+
