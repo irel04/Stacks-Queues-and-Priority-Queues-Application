12ae4c3e00e5ff1fe6fdbc90cc95c4c69f86c8ac
Mark Lhester Agellon <kianirel56@gmail.com>
2022-12-16 14:05:06 +0800

    import: argparse annd LifoQueue, PriorityQueue, Queue

--- Dijkstra_Shortest_Path Demo.py
+++ Dijkstra_Shortest_Path Demo.py
@@ -9,5 +9,13 @@ city2 = nodes["edinburgh"]
 def distance(weights):
     return float(weights["distance"])
 
+print("\n\nWeight-Distance sort Approach:")
 for city in dijkstra_shortest_path(graph, city1, city2, distance):
+    print(city.name)
+
+print("\n\nWeight sort Approach:")
+def weight(node1, node2, weights):
+    return distance(weights)
+
+for city in nx.dijkstra_path(graph, city1, city2, weight):
     print(city.name)
